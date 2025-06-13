6e0098e2f4e66b10f3291cb756011a81a06a9cd4
Vladislava <dyachenkov1006@gmail.com>
2025-06-13 11:57:58 +0100

    initial commit

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <p>
+      hello
+    </p>
+  `,
+  styles: ``
+})
+export class Header {
+
+}
